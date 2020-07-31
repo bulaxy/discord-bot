206a6304aed34aed5ba9d54928f06c9be520a8df
Bulaxy <rogerhiufungcheng@gmail.com>
2020-07-31 11:55:42 +1000

    Music Bot Fixes and adding simple customisation

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,21 +1,22 @@
 const { Command } = require('discord.js-commando');
+const Discord = require('discord.js');
 const ytdl = require("ytdl-core");
 const ytpl = require('ytpl');
 const ytsr = require('ytsr');
-
+const MUSIC_CONSTANTS = require('../../botmessagers/music.json')
 module.exports = class PlayCommand extends Command {
 	constructor(client) {
 		super(client, {
-			name: 'play',
+			name: MUSIC_CONSTANTS.play,
 			group: 'music',
 			memberName: 'play',
-			description: 'Play Music',
+			description: MUSIC_CONSTANTS.play_description,
 			guildOnly: true,
 			clientPermissions: ['SPEAK', 'CONNECT'],
 			args: [
 				{
 					key: 'query',
-					prompt: 'What song or playlist would you like to listen to?',
+					prompt: MUSIC_CONSTANTS.play_prompt,
 					type: 'string',
 					validate: function (query) {
 						return query.length > 0 && query.length < 200;
@@ -26,66 +27,78 @@ module.exports = class PlayCommand extends Command {
 	}
 
 	async run(message, { query }) {
-		if (!message.member.voice.channel) { //check user voice channel
-			// message.say(CONSTANTSTEXT.MUST_JOIN_VOICE_CHANNEL)
-			message.say('not in voice chanel')
+		//First check whether the user is in a voice channel, TODO: if in voice channel,make sure is the same as the one the bot is in.
+		if (!message.member.voice.channel) {
+			message.say(MUSIC_CONSTANTS.user_not_in_voice_channel)
 			return null
 		}
+		const sentMsg = await message.say(MUSIC_CONSTANTS.searching_for_song)
+		var ytls = []
 
-		if (
-			query.match(
-				/^(?!.*\?.*\bv=)https:\/\/www\.youtube\.com\/.*\?.*\blist=.*$/
-			)
-		) { //if is playlist
-			var ytl = await ytpl(query)
-			ytl.items.forEach(item => {
-				message.guild.musicData.queue.push({ //add each song in playist to queue
-					url: item.url,
-					title: item.title,
-					lengthSecond: getSecond(item.duration)
-				})
+		switch (true) {
+			case (query.includes('.com/playlist')): //If it is a playlist
+				// sentMessage.edit(MUSIC_CONSTANT.searching_for_playlist);
+				sentMsg.edit('playlist might take longer');
+				var ytplInfo = await ytpl(query)
+				ytplInfo.items.forEach(item => ytls.push(item.url))
+				break;
+			case ((!query.includes('https://') && !query.includes('http://'))): //If it is a search request
+				var ytsrInfo = await ytsr(query)
+				ytls.push(ytsrInfo.items.find(item => item.type == 'video').link)
+				break;
+			default:
+				ytls = [query]
+		}
+		for (var i = 0; i < ytls.length; i++) {
+			var basicInfo = await ytdl.getBasicInfo(ytls[i]) //get basic info,
+			message.guild.musicData.queue.push({ //add to queue
+				url: ytls[i],
+				title: basicInfo.player_response.videoDetails.title,
+				lengthSecond: basicInfo.player_response.videoDetails.lengthSeconds,
+				thumbnail: basicInfo.player_response.videoDetails.thumbnail.thumbnails[0].url,
+				details: basicInfo.player_response.videoDetails, //in case, for future use or reference
 			})
-		} else if (!query.match(/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+/)) {
-			var searchTerm = message.content.substring(query); //TODO Check
-			var ytl = await ytsr(searchTerm)
-			ytl = ytl.items.find(item => item.type == 'video').link
-		} else {
-			//if not a playlist or a search, it is just a single video link
-			ytl = query
 		}
-		console.log("***** received Play, loading basic Info")
-		var basicInfo = await ytdl.getBasicInfo(ytl) //get basic info,
-		message.guild.musicData.queue.push({ //add to queue
-			url: ytl,
-			title: basicInfo.player_response.videoDetails.title,
-			lengthSecond: basicInfo.player_response.videoDetails.lengthSeconds,
-		})
-		if (message.guild.musicData.nowPlaying) { //if already playing, 
-			// message.say(CONSTANTSTEXT.ADD_TO_QUEUE)
-			message.say(`***** Added to Queue`)
-		}
-		if (_.isUndefined(message.guild.voice) || !message.guild.voice.channelID) { //if not in voice channel, start the play function 
+
+		if (_.isUndefined(message.guild.voice) || _.isNull(message.guild.voice.channelID) || !message.guild.musicData.isPlaying) { //if already playing, 
+			//if not in voice channel, start the play function 
 			message.member.voice.channel
 				.join()
-				.then(function (connection) {
-					PlayCommand.play(message)
-				})
+				.then(() => PlayCommand.play(message, sentMsg))
+		} else {
+			sentMsg.edit(MUSIC_CONSTANTS.added_to_queue)
 		}
 	}
 
-	static play(message) {
-		const classThis = this; // use classThis instead of 'this' because of lexical scope below
-		// var nextPlayingText = (_.isUndefined(server.queue[1])) ? "" : CONSTANTSTEXT.NEXT_SONG + server.queue[1].title //append next song.
-		// message.channel.send(CONSTANTSTEXT.NOW_PLAYING + server.queue[0].title + nextPlayingText)
-		message.guild.musicData.dispatcher = message.guild.voice.connection.play(ytdl(message.guild.musicData.queue[0].url, { quality: 'lowest', filter: "audioonly" })) //set dispatcher to the song received.
-		message.guild.musicData.nowPlaying = true
-		message.guild.musicData.queue.shift();	//Remove current song from queue and add it to currentSong
+	static play(message, sentMsg = null) {
+		const classThis = this;
+		message.guild.musicData.nowPlaying = message.guild.musicData.queue.shift()
+		message.guild.musicData.isPlaying = true
+		message.guild.musicData.dispatcher = message.guild.voice.connection.play(ytdl(message.guild.musicData.nowPlaying.url, { quality: 'lowest', filter: "audioonly" })) //set dispatcher to the song received.
+
+		const nowPlayingEmbed = new Discord.MessageEmbed()
+			.setColor(CONSTANTS.embedColor)
+			.setTitle(MUSIC_CONSTANTS.now_play_title + message.guild.musicData.nowPlaying.title)
+			.setURL(message.guild.musicData.nowPlaying.url)
+			.setImage(message.guild.musicData.nowPlaying.thumbnail)
+
+		if (_.isNull(sentMsg)) {
+			message.say(nowPlayingEmbed)
+		} else {
+			sentMsg.edit(nowPlayingEmbed)
+		}
 		message.guild.musicData.dispatcher.on("finish", function () { //Listen to when song finish.
+			message.guild.musicData.isPlaying = false
 			if (message.guild.musicData.queue[0]) { //if queue is not empty, play the next song
 				classThis.play(message)
 			} else {
-				// message.channel.send(CONSTANTSTEXT.NO_SONG_IN_QUEUE)
-				message.guild.voice.connection.disconnect() //Leave channel
+				message.say(MUSIC_CONSTANTS.no_song_in_queue)
+				message.guild.musicData.nowPlaying = null
+				message.guild.musicData.dispatcher = null
+				message.guild.musicData.isPlaying = false
+				if (message.guild.me.voice.channel) {
+					return message.guild.me.voice.channel.leave();
+				}
 			}
 		});
 	}
